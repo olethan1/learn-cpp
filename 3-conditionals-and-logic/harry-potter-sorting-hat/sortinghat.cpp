1afdf29976ae4e0789951db861cf9269add8cd4c
olethan1 <72436994+olethan1@users.noreply.github.com>
2020-10-06 04:06:57 -0400

    Update sortinghat.cpp
    I definitely went a little overboard on this one, but hopefully, it will get added to the project! My Codecademy username is @olethan.

--- 3-conditionals-and-logic/harry-potter-sorting-hat/sortinghat.cpp
+++ 3-conditionals-and-logic/harry-potter-sorting-hat/sortinghat.cpp
@@ -1,153 +1,244 @@
 #include <iostream>
+#include <exception>
+#include <cstdlib>  
 
-int main() {
+void myterminate () {
+  std::cerr << "Input was out of range!\n";
+  abort();
+}
 
-  int gryffindor = 0;
-  int hufflepuff = 0;
-  int ravenclaw = 0;
-  int slytherin = 0;
+int main(void) {
 
-  int answer1, answer2, answer3, answer4;
+int gryffindor = 0;
+int hufflepuff = 0;
+int ravenclaw = 0;
+int slytherin = 0;
+int answer1 = 0;
+int answer2 = 0;
+int answer3 = 0;
+int answer4 = 0;
 
-  std::cout << "===============\n";
-  std::cout << "The Sorting Hat\n";
-  std::cout << "===============\n\n";
+std::cout << "=====================\n";
+std::cout << "The Sorting Hat Quiz!\n";
+std::cout << "=====================\n\n";
 
-  // ~~~~~~~~~~ Question 1 ~~~~~~~~~~
+std::cout << "Q1) When I'm dead, I want people to remember me as:\n\n";
 
-  std::cout << "Q1) When I'm dead, I want people to remember me as:\n\n";
+std::cout << "  1) The Good\n";
+std::cout << "  2) The Great\n";
+std::cout << "  3) The Wise\n";
+std::cout << "  4) The Bold\n\n";
+std::cout << "Enter answer: ";
+std::cin >> answer1;
+std::cout << "\n";
 
-  std::cout << "  1) The Good\n";
-  std::cout << "  2) The Great\n";
-  std::cout << "  3) The Wise\n";
-  std::cout << "  4) The Bold\n\n";
-
-  std::cout << "Enter your answer (1-4): ";
-  std::cin >> answer1;
-
-  if (answer1 == 1)
+switch (answer1) {
+  case 1:
     hufflepuff++;
-  else if (answer1 == 2)
+    break;
+  case 2:
     slytherin++;
-  else if (answer1 == 3)
+    break;
+  case 3:
     ravenclaw++;
-  else if (answer1 == 4)
+    break;
+  case 4:
     gryffindor++;
+    break;
+  default:
+    std::cout << "Invalid input!\nPlease enter a valid number between 1-4: ";
+    std::cin >> answer1;
+    std::cout << "\n";
 
-  // ~~~~~~~~~~ Question 2 ~~~~~~~~~~
+    switch (answer1) {
+      case 1:
+        hufflepuff++;
+        break;
+      case 2:
+        slytherin++;
+        break;
+      case 3:
+        ravenclaw++;
+        break;
+      case 4:
+        gryffindor++;
+        break;
+      default:
+        std::cout << "Invalid answer!\nProgram ending...\n";
+        std::set_terminate (myterminate);
+        throw 0;
+        return 0;
+    }
+    break;
+}
 
-  std::cout << "\nQ2) Dawn or Dusk?\n\n";
+std::cout << "Q2) Dawn or Dusk?\n\n";
 
-  std::cout << "  1) Dawn\n";
-  std::cout << "  2) Dusk\n\n";
+std::cout << "  1) Dawn\n";
+std::cout << "  2) Dusk\n";
+std::cout << "Enter answer: ";
+std::cin >> answer2;
+std::cout << "\n";
 
-  std::cout << "Enter your answer (1-2): ";
-  std::cin >> answer2;
-
-  if (answer2 == 1)
-  {
-
+switch (answer2) {
+  case 1:
     gryffindor++;
     ravenclaw++;
-
-  }
-  else if (answer2 == 2)
-  {
-
+    break;
+  case 2:
     hufflepuff++;
     slytherin++;
+    break;
+  default:
+    std::cout << "Invalid input!\nPlease enter a valid number between 1-2: ";
+    std::cin >> answer2;
+    switch (answer2) {
+      case 1:
+        gryffindor++;
+        ravenclaw++;
+        break;
+      case 2:
+        hufflepuff++;
+        slytherin++;
+        break;
+      default:
+        std::cout << "Invalid answer!\nProgram ending...\n";
+        std::set_terminate (myterminate);
+        throw 0;
+        return 0;
+    }
+    break;
+}
 
-  }
-  else
-  {
+std::cout << "Q3) Which kind of instrument most pleases your ear?\n\n";
 
-    std::cout << "Invalid input\n";
+std::cout << "  1) The violin\n";
+std::cout << "  2) The trumpet\n";
+std::cout << "  3) The piano\n";
+std::cout << "  4) The drum\n\n";
+std::cout << "Enter answer: ";
+std::cin >> answer3;
+std::cout << "\n";
 
-  }
-
-  // ~~~~~~~~~~ Question 3 ~~~~~~~~~~
-
-  std::cout << "\nQ3) Which kind of instrument most pleases your ear?\n\n";
-
-  std::cout << "  1) The violin\n";
-  std::cout << "  2) The trumpet\n";
-  std::cout << "  3) The piano\n";
-  std::cout << "  4) The drum\n\n";
-
-  std::cout << "Enter your answer (1-4): ";
-  std::cin >> answer3;
-
-  if (answer3 == 1)
+switch (answer3) {
+  case 1:
     slytherin++;
-  else if (answer3 == 2)
+    break;
+  case 2:
     hufflepuff++;
-  else if (answer3 == 3)
+    break;
+  case 3:
     ravenclaw++;
-  else if (answer3 == 4)
+    break;
+  case 4:
     gryffindor++;
+    break;
+  default:
+    std::cout << "Invalid input!\nPlease enter a valid number between 1-4: ";
+    std::cin >> answer3;
 
-  // ~~~~~~~~~~ Question 4 ~~~~~~~~~~
+    switch(answer3) {
+      case 1:
+        slytherin++;
+        break;
+      case 2:
+        hufflepuff++;
+        break;
+      case 3:
+        ravenclaw++;
+        break;
+      case 4:
+        gryffindor++;
+        break;
+      default:
+        std::cout << "Invalid answer!\nProgram ending...\n";
+        std::set_terminate (myterminate);
+        throw 0;
+        return 0;
+    }
+   break; 
+}
 
-  std::cout << "\nQ4) Which road tempts you the most?\n\n";
+std::cout << "Q4) Which road tempts you most?\n\n";
 
-  std::cout << "  1) The wide, sunny grassy lane\n";
-  std::cout << "  2) The narrow, dark, lantern-lit alley\n";
-  std::cout << "  3) The twisting, leaf-strewn path through woods\n";
-  std::cout << "  4) The cobbled street lined (ancient buildings)\n\n";
+std::cout << "  1) The wide, sunny grassy lane\n";
+std::cout << "  2) The narrow, dark, lantern-lit alley\n";
+std::cout << "  3) The twisting, leaf-strewn path through woods\n";
+std::cout << "  4) The cobbled street lined (ancient buildings)\n\n";
+std::cout << "Enter answer: ";
+std::cin >> answer4;
+std::cout << "\n";
 
-  std::cout << "Enter your answer (1-4): ";
-  std::cin >> answer4;
-
-  if (answer4 == 1)
+switch (answer4) {
+  case 1:
     hufflepuff++;
-  else if (answer4 == 2)
+    break;
+  case 2:
     slytherin++;
-  else if (answer4 == 3)
+    break;
+  case 3:
     gryffindor++;
-  else if (answer4 == 4)
+    break;
+  case 4:
     ravenclaw++;
+    break;
+  default:
+    std::cout << "Invalid input!\nPlease enter a valid number between 1-4: ";
+    std::cin >> answer4;
 
-  // ========== Sorting ==========
+    switch (answer4) {
+      case 1:
+        hufflepuff++;
+        break;
+      case 2:
+        slytherin++;
+        break;
+      case 3:
+        gryffindor++;
+        break;
+      case 4:
+        ravenclaw++;
+        break;
+      default:
+        std::cout << "Invalid answer!\nProgram ending...\n";
+        std::set_terminate (myterminate);
+        throw 0;
+        return 0;
+    }
+  break;
+}
 
-  std::cout << "\nCongrats on being sorted into... ";
+std::string house;
+int max = 0;
 
-  int max = 0;
-  std::string house;
+if (gryffindor > max) {
 
-  if (gryffindor > max)
-  {
+  max = gryffindor;
+  house = "Gryffindor";
 
-    max = gryffindor;
-    house = "Gryffindor";
+}
 
-  }
+if (hufflepuff > max) {
 
-  if (hufflepuff > max)
-  {
+  max = hufflepuff;
+  house = "Hufflepuff";
 
-    max = hufflepuff;
-    house = "Hufflepuff";
+}
 
-  }
+if (ravenclaw > max) {
 
-  if (ravenclaw > max)
-  {
+  max = ravenclaw;
+  house = "Ravenclaw";
 
-    max = ravenclaw;
-    house = "Ravenclaw";
+}
 
-  }
+if (slytherin > max) {
 
-  if (slytherin > max)
-  {
+  max = slytherin;
+  house = "Slytherin";
 
-    max = slytherin;
-    house = "Slytherin";
+}
 
-  }
-
-  std::cout << house << "!\n";
-
-  return 0;
+std::cout << house << "!\n";
 
 }
